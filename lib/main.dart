ca87c558d652b93605880f91a96aeb9af8f46390
Your Name <you@example.com>
2023-03-15 17:42:40 +0100

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:bucketlist/widget_tree.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const WidgetTree(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+/*
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -57,7 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter += 2;
     });
   }
 
@@ -108,8 +136,9 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.chat),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+*/
